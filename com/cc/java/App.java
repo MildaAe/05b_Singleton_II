ded6b861e50aafb28cefeabfbff6c63f1f15a668
Peter Schmidt <schmidt.multimedia@gmail.com>
2022-03-16 14:11:46 +0100

    Add files via upload

---
+++ com/cc/java/App.java
@@ -0,0 +1,17 @@
+package com.cc.java;
+
+public class App {
+  public static void main(String[] args) {
+		
+		// Namenslisten --> Arrays
+		String [] catNames = {"Grizabella","Alonzo","RumTumTugger","Suzanne"};
+		String [] dogNames = {"Fifi","Boo","Cassie","Bonzo"};
+
+
+
+	}
+
+    private static void output(String outputStr) {
+        System.out.println(outputStr);
+    }
+}
